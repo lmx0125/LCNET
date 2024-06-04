5ab539052582764e2a6a419ad27598c516610076
lmx0125 <121705055+lmx0125@users.noreply.github.com>
2024-06-04 10:59:57 +0800

    Add files via upload

--- UDP/udp.cpp
+++ UDP/udp.cpp
@@ -1,1 +1,6 @@
+#include "udp.h"
 
+unsigned long UDP::register_new_device(const char* addr, UINT port) {
+
+	return 111;
+}
--- UDP/udp.h
+++ UDP/udp.h
@@ -1,1 +1,27 @@
+#ifndef _LC_UDP_
+#define _LC_UDP_
 
+#include <WinSock2.h> 
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+#include <vector>
+#include <signal.h>
+#include <random>
+#include <time.h>
+#include <future>
+#include <mutex>
+#include <atlstr.h>
+#include "../log.h"
+
+extern struct device_struct;
+
+class UDP : LOG {
+public:
+	unsigned long register_new_device(const char* addr, UINT port);
+	
+	std::vector<device_struct*> device_list;
+};
+
+#endif
