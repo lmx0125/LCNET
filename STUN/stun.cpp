dfdb8e17fd2e1403e2831828f218213d17ae9605
lmx0125 <121705055+lmx0125@users.noreply.github.com>
2024-08-13 23:55:06 +0800

    Add files via upload

---
+++ STUN/stun.cpp
@@ -0,0 +1,74 @@
+#include "stun.h"
+
+STUN::STUN(NET* net) {
+	this->net = net;
+}
+
+void STUN::build_stun_package_header(uint16_t msg_type, uint16_t att_type) {
+	memset(header, 0, sizeof(header));
+	header[0] = (msg_type >> 8) & 0xFF;
+	header[1] = msg_type & 0xFF;
+	header[2] = 0; // Length (to be updated later) 
+	header[3] = 0;
+	memcpy(header + 4, &MAGIC_COOKIE, sizeof(MAGIC_COOKIE));
+	gen_transaction_id();
+	for (size_t i = 0; i < 12; i++)
+		header[i + 8] = transaction_id[i];
+	Sleep(1);
+}
+
+void STUN::gen_transaction_id() {
+	srand((unsigned int)time(nullptr));
+	for (size_t i = 0; i < 12; i++)
+		transaction_id[i] = rand() % 256;
+}
+
+void STUN::send_stun_bind_request(const char* addr, UINT port) {
+	// get old callback details
+	old_parameter = net->udp.pass_parameter;
+	old_callback_func = net->udp.callback_func;
+	net->udp.pass_parameter.clear(); 
+	
+	if (recv_data != nullptr)
+		delete[] recv_data;
+	recv_data = new uint8_t[256];
+	net->udp.set_callback_func_pass_parameter(recv_data);
+	net->udp.set_callback_func_pass_parameter(this);
+	// set callback
+	this->net->udp.set_udp_package_recv_callback(stun_recv_callback);
+	STUN_SERVER_ID = this->net->udp.register_new_device(addr, port);
+	char msg[ARRAYSIZE(header)];
+	for (size_t i = 0; i < ARRAYSIZE(header); i++)
+		msg[i] = (char)header[i];
+	this->net->udp.send(msg, STUN_SERVER_ID,ARRAYSIZE(header));
+}
+
+void STUN::stun_recv_callback(char* data, long buffer_size, device_struct*, int status, UDP* udp, std::vector<void*> else_parameter) {
+	STUN* stun = (STUN*)(else_parameter[1]);
+	memcpy(else_parameter[0], data, buffer_size);
+	//else_parameter[0] = (uint8_t*)data;
+	uint8_t* test_array = (uint8_t*)data;
+	for (long i = 0; i < buffer_size; i++) {
+		std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(test_array[i]) << " ";
+	}
+	std::cout << std::endl;
+	stun->stun_callback_clear();
+}
+
+void STUN::stun_callback_clear() {
+	this->net->udp.callback_func = old_callback_func;
+	net->udp.pass_parameter.clear();
+	this->net->udp.pass_parameter = old_parameter;
+}
+
+uint16_t STUN::get_end_port() {
+	return recv_data[26] * 256 + recv_data[27];
+}
+
+std::string STUN::get_end_IP() {
+	std::string ret_str = std::to_string(recv_data[28]) + "."
+		+ std::to_string(recv_data[29]) + "."
+		+ std::to_string(recv_data[30]) + "."
+		+ std::to_string(recv_data[31]);
+	return ret_str;
+}
---
+++ STUN/stun.h
@@ -0,0 +1,57 @@
+#ifndef _STUN_
+#define _STUN_
+
+#include "../net.h"
+#include "../UDP/udp.h"
+#include <iomanip>
+
+const uint32_t MAGIC_COOKIE = 0x2112A442;
+#define STUN_HEADER_SIZE 20
+
+enum message_type {
+    Binding_Request = 0x0001,
+    Binding_Response = 0x0101,
+    Binding_Error_Response = 0x0111,
+    Shared_Secret_Request = 0x0002,
+    Shared_Secret_Response = 0x0102,
+    Shared_Secret_Error_Response = 0x0112
+};
+
+enum attribute_type {
+    mapped_address = 0x0001,
+    response_address = 0x0002,
+    change_request = 0x0003,
+    source_address = 0x0004,
+    changed_address = 0x0005,
+    username = 0x0006,
+    password = 0x0007,
+    message_integrity = 0x0008,
+    error_code = 0x0009,
+    unknown_attributes = 0x000a,
+    reflected_from = 0x000b
+};
+
+class STUN {
+public:
+    STUN(NET* net);
+    ~STUN() { std::cout << "aasdfsdfasf" << std::endl; }
+    void build_stun_package_header(uint16_t msg_type, uint16_t att_type);
+    void gen_transaction_id();
+    void send_stun_bind_request(const char* addr, UINT port);
+    static void stun_recv_callback(char* data, long buffer_size, device_struct*, int status, UDP* udp, std::vector<void*>);
+    void stun_callback_clear();
+
+    uint16_t get_end_port();
+    std::string get_end_IP();
+
+    uint8_t* recv_data = nullptr;
+    NET* net;
+    uint8_t header[STUN_HEADER_SIZE];
+    uint8_t transaction_id[12] = { 0 };
+    ul STUN_SERVER_ID = 0;
+    std::vector<void*> old_parameter;
+    package_recv_callback_func old_callback_func = nullptr;
+
+};
+
+#endif
