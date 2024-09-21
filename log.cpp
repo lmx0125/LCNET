6c1f06ab89de9e46456d7511b155130474521b5f
lmx0125 <121705055+lmx0125@users.noreply.github.com>
2024-09-21 14:00:06 +0800

    Delete log.cpp

--- log.cpp
+++
@@ -1,7 +0,0 @@
-#include "log.h"
-
-void LOG::Show_log(std::string log_type, std::string log_msg) {
-	std::string str;
-	str = "[" + log_type + "] [" + log_msg + "]\n";
-	std::cout << str << std::flush;
-}
