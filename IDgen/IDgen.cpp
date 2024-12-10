8e336a159217f9944adfb68edc6373442463ee9d
lmx0125 <121705055+lmx0125@users.noreply.github.com>
2024-12-10 08:08:57 +0800

    add uuid generate module

--- IDgen/IDgen.cpp
+++ IDgen/IDgen.cpp
@@ -15,3 +15,19 @@ void IDgen::deleteID(unsigned long ID) {
 	auto val = std::remove(ID_list.begin(), ID_list.end(), ID);
 	ID_list.erase(val, ID_list.end());
 }
+
+std::string IDgen::getUUID() {
+	char uid_c[38];
+#ifdef _WIN32
+	GUID uid;
+	CoCreateGuid(&uid);
+	sprintf(uid_c, "%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X",
+		uid.Data1, uid.Data2, uid.Data3, uid.Data4[0], uid.Data4[1], uid.Data4[2], uid.Data4[3], uid.Data4[4], uid.Data4[5], uid.Data4[6], uid.Data4[7]
+	);
+#else
+	uuid_t uid;
+	uuid_generate(uid);
+	uuid_unparse(uid, uid_c);
+#endif
+	return std::string(uid_c);
+}
--- IDgen/IDgen.h
+++ IDgen/IDgen.h
@@ -1,6 +1,12 @@
 #ifndef _IDGEN_H_
 #define _IDGEN_H_
 
+#ifdef _WIN32
+#include <combaseapi.h>
+#else
+#include <uuid/uuid.h>
+#endif
+
 #include <vector>
 #include <time.h>
 #include <mutex>
@@ -12,6 +18,9 @@ public:
 	IDgen() = default;
 	unsigned long generateID();
 	void deleteID(unsigned long ID);
+
+	static std::string getUUID();
+
 private:
 	std::vector<unsigned long> ID_list;
 	std::mutex mtx;
